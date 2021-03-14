99adc634750b8b095a69c9a6301dd1c05ce9d2f5
Duc DInh <ducdinh-codehub>
2021-03-14 11:15:33 +0700

    Newest version report 04 Signal

--- 04_report_work_signal/04.practical.work.shell.signal.c
+++ 04_report_work_signal/04.practical.work.shell.signal.c
@@ -4,58 +4,55 @@
 #include<stdbool.h>
 #include<signal.h>
 #include<stdlib.h>
-void handler(int signal_num){
-    printf("Signal %d => ", signal_num);
-    switch(signal_num){
-        case SIGTSTP:
-            printf("Pause\n");
-            break;
-        case SIGINT:
-        case SIGTERM:
-            printf("Terminated\n");
-            exit(0);
-            break;
-    }
-}
+#include <sys/types.h>
+#include <sys/wait.h>
+int childpid = 0;
+void handler(int);
 int main(void){
-    /*
-    // Basic example.
-    // ctrl z
-    signal(SIGTSTP,handler);
-    // ctrl c or killed
-    signal(SIGINT,handler);
-    signal(SIGTERM,handler);
-    while(1) {
-        sleep(1);
-        printf(".\n");
-    }*/
 	printf("Main before fork\n");
 	char command[100];
 	char parameter[100];
+    signal(SIGTSTP, handler); // Register signal
+    signal(SIGTERM, handler);
+    signal(SIGINT, handler);
 	while(true){
 		printf("I'm parent\n");
 		printf("Signal handler example\n");
-		if(strcmp(command,"break")==0 || strcmp(parameter,"break")==0){
-			break;
-		}
+		printf("*NOTE: type '-quit' to close shell !\n");
 		printf("Type command:\n");
 		scanf("%s",command);
 		printf("Type parameter:\n");
 		scanf("%s",parameter);
 		printf("Your command %s %s\n",command,parameter);
+        if(strcmp(command,"-quit")==0 || strcmp(parameter,"-quit")==0){
+            break;
+        }
 		int pid = fork();
-		printf("Process id: %d",pid);
-		if(pid == 0){
-			printf("I'm child after fork(), launching!");
-			char *args[] = {command, parameter, NULL};
-			execvp(command,args);
-            kill(pid, SIGKILL);
-            printf("Finished launching ps -ef\n");
+		printf("Process id: %d\n",pid);
+		if(pid){
+            printf("I'm parent wait and I'm waiting my child\n");
+		    childpid = pid;
+		    waitpid(pid, NULL, 0);
+		}else{
+		    printf("I'm a child\n");
+            char *args[] = {command, parameter, NULL};
+            execvp(command,args);
 		}
-        signal(SIGTSTP, handler);
-        signal(SIGTERM, handler);
-        printf("Finished launching ps -ef\n");
 	}
-	printf("I'm parent mode");
 	return 0;
+}
+void handler(int signal_num){
+    printf("Signal %d => ", signal_num);
+    switch(signal_num){
+        case SIGTSTP:
+            printf("Pause\n");
+            kill(childpid, SIGTSTP);
+            break;
+        case SIGINT:
+        case SIGTERM:
+            printf("Terminated\n");
+            kill(childpid, SIGTERM);
+            exit(0);
+            break;
+    }
 }
