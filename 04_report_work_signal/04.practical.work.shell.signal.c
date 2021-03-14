ecff4bd3e8ca4b293cd2dbd65d7b77eb87320721
Duc DInh <ducdinh-codehub>
2021-03-14 11:18:25 +0700

    Newest version report 04 Signal

--- 04_report_work_signal/04.practical.work.shell.signal.c
+++ 04_report_work_signal/04.practical.work.shell.signal.c
@@ -9,37 +9,37 @@
 int childpid = 0;
 void handler(int);
 int main(void){
-	printf("Main before fork\n");
-	char command[100];
-	char parameter[100];
+    printf("Main before fork\n");
+    char command[100];
+    char parameter[100];
     signal(SIGTSTP, handler); // Register signal
     signal(SIGTERM, handler);
     signal(SIGINT, handler);
-	while(true){
-		printf("I'm parent\n");
-		printf("Signal handler example\n");
-		printf("*NOTE: type '-quit' to close shell !\n");
-		printf("Type command:\n");
-		scanf("%s",command);
-		printf("Type parameter:\n");
-		scanf("%s",parameter);
-		printf("Your command %s %s\n",command,parameter);
+    while(true){
+        printf("I'm parent\n");
+        printf("Signal handler example\n");
+        printf("*NOTE: type '-quit' to close shell !\n");
+        printf("Type command:\n");
+        scanf("%s",command);
+        printf("Type parameter:\n");
+        scanf("%s",parameter);
+        printf("Your command %s %s\n",command,parameter);
         if(strcmp(command,"-quit")==0 || strcmp(parameter,"-quit")==0){
             break;
         }
-		int pid = fork();
-		printf("Process id: %d\n",pid);
-		if(pid){
+        int pid = fork();
+        printf("Process id: %d\n",pid);
+        if(pid){
             printf("I'm parent wait and I'm waiting my child\n");
-		    childpid = pid;
-		    waitpid(pid, NULL, 0);
-		}else{
-		    printf("I'm a child\n");
+            childpid = pid;
+            waitpid(pid, NULL, 0);
+        }else{
+            printf("I'm a child\n");
             char *args[] = {command, parameter, NULL};
             execvp(command,args);
-		}
-	}
-	return 0;
+        }
+    }
+    return 0;
 }
 void handler(int signal_num){
     printf("Signal %d => ", signal_num);
